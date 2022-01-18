2364997ed5261496c8b426dd3451bd32f1cca71c
mesa <mesa_lu@hotmail.com>
2022-01-18 22:54:22 +0800

    vue-demo

---
+++ vue-demo/src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const Home = () => import('@/views/Home.vue')
+const About = () => import('@/views/About.vue')
+const Mm = () => import('@/views/Mm.vue')
+
+const routes = [
+  {path: '/',redirect:'home'},
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: 'about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: About
+      },
+      {
+        path: 'mm',
+        name: 'Mm',
+        component: Mm
+      }
+    ],
+  },
+
+//   {
+//     path: '/about',
+//     name: 'About',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: About
+//   },
+//   {
+//     path: '/mm',
+//     name: 'Mm',
+//     component: Mm
+// }, 
+  
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ vue-demo/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/home/about">About</router-link> |
+      <router-link to="/home/mm">MM</router-link>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p>-------下面是home的route-view-------</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import {router} from '@/router/index'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return{
+      router:router
+    }
+  },
+  created() {
+    console.log(router)
+  },
+}
+</script>
